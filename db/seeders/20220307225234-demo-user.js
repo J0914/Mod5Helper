099a80f03a803d6eff6862473bc7586f65469d16
J0914 <jordynsechrist@gmail.com>
2022-03-07 18:01:09 -0600

    setup api routes for session - login complete

---
+++ db/seeders/20220307225234-demo-user.js
@@ -0,0 +1,20 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      email: { [Op.in]: ['demo@aa.io'] }
+    }, {});
+  }
+};
